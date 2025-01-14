cffdb1b92c3bf7a28648919a63478d0985a585de
CoolNight99 <timmyscottmy@gmail.com>
2025-01-14 14:33:23 +0800

    fixed asterisk colors

--- docs/script.js
+++ docs/script.js
@@ -10,67 +10,82 @@ const consentCheckboxRequired = document.querySelector(".consent-checkbox-requir
 const submitBtn = document.querySelector(".submit-btn");
 const successDiv = document.querySelector(".success-div");
 
+const asterisk = document.querySelector(".asterisk");
+
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 function validateInput(inputType) {
     const input = document.getElementById(inputType)
     const inputRequired = document.querySelector(`.${inputType}-required`);
+    const asterisk = document.querySelector(`.${inputType}-asterisk`);
 
     if (input.value) {
         inputRequired.style.display = "none";
+        asterisk.style.color = "#64e416";
         return true;
     }
 
     else {
         inputRequired.style.display = "inline";
+        asterisk.style.color = "#d73c3c";
         return false;
     }
 }
 
 function validateEmail(email) {
     const emailRequired = document.querySelector(".email-required");
+    const asterisk = document.querySelector(".email-asterisk");
 
     if (!email.value) {
         emailRequired.style.display = "inline";
+        asterisk.style.color = "#d73c3c";
         return false;
     }
 
     if (emailRegex.test(email.value)) {
         emailRequired.style.display = "none";
+        asterisk.style.color = "#64e416";
         return true;
     }
 
     else {
         emailRequired.style.display = "inline";
+        asterisk.style.color = "#d73c3c";
         return false;
     }
 }
 
 function validateQueryType(queryTypeButtons) {
     const isChecked = Array.from(queryTypeButtons).some((button) => button.checked);
+    const asterisk = document.querySelector(".query-type-asterisk");
 
     if (isChecked) {
         queryTypeRequired.style.display = "none";
+        asterisk.style.color = "#64e416";
         return true;
     } 
     
     else {
        queryTypeRequired.style.display = "inline";
+       asterisk.style.color = "#d73c3c";
        return false;
     }
 }
 
 function validateConsentCheckbox(consentCheckbox) {
     const isChecked = consentCheckbox.checked;
+    const asterisk = document.querySelector(".consent-checkbox-asterisk");
 
     if (isChecked) {
         consentCheckboxRequired.style.display = "none";
+        asterisk.style.color = "#64e416";
         return true;
     } 
     
     else {
         consentCheckboxRequired.style.display = "inline";
-       return false;
+        asterisk.style.color = "#d73c3c";
+        return false;
     }
 }
 
